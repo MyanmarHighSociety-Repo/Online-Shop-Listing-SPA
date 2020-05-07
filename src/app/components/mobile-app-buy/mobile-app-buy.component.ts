f36d2a50c19ae12928ceacdba0232a4a227c08f5
nanghtwekham <nanghtwekham.mhs@gmail.com>
2020-05-07 17:04:30 +0630

    shop app

--- src/app/components/mobile-app-buy/mobile-app-buy.component.ts
+++ src/app/components/mobile-app-buy/mobile-app-buy.component.ts
@@ -10,10 +10,37 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class MobileAppBuyComponent implements OnInit {
   constructor(private router: Router) {}
 
+  public show: boolean = false;
+  public buttonName: any = 'အကုန်ကြည့်ရန်';
+
   ngOnInit() {}
+
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    toggle() {
+      this.show = !this.show;
+      // CHANGE THE NAME OF THE BUTTON.
+      if (this.show) {
+      this.buttonName = 'View Less';
+      }
+      else {
+      this.buttonName = 'အကုန်ကြည့်ရန်';
+    }
+  }
+
 
   goToSearch() {
     this.router.navigate(['/shop-search']);
+  }
+  goToShopAdd() {
+    this.router.navigate(['/add-shop-step-one']);
+  }
+
+  goToContactUs() {
+    this.router.navigate(['/contact-us']);
   }
 }
 
