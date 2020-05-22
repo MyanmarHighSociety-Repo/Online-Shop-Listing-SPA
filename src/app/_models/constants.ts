047ba1018fc0f0ecd09c72f1b958046c2b2f21b2
Arkaar <arkarmin.mhs@gmail.com>
2020-05-22 15:44:04 +0630

    Arkar

--- src/app/_models/constants.ts
+++ src/app/_models/constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
-    static readonly API_URL_PREFIX = 'http://3.1.81.62:81';                // For local use
-    // static readonly API_URL_PREFIX = 'https://localhost:5001';
+    // static readonly API_URL_PREFIX = 'http://3.1.81.62:81';                // For local use
+    static readonly API_URL_PREFIX = 'https://localhost:5001';
 }
